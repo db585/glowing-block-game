dfe53619ea87ad7e90a9757dc5103dbbcbb80b9c
Denis Irkhin <denis555@me.com>
2019-01-16 15:56:05 -0500

    Start the proj

--- js/kittens.js
+++ js/kittens.js
@@ -14,30 +14,30 @@ let LEFT_ARROW_CODE = 37
 let RIGHT_ARROW_CODE = 39
 
 // These two constants allow us to DRY
-let MOVE_LEFT = "left"
-let MOVE_RIGHT = "right"
+let MOVE_LEFT = 'left'
+let MOVE_RIGHT = 'right'
 
 // Preload game images
-let imageFilenames = ["enemy.png", "stars.png", "player.png"]
+let imageFilenames = ['enemy.png', 'stars.png', 'player.png']
 let images = {}
 
-imageFilenames.forEach(function(imgName) {
-  let img = document.createElement("img")
-  img.src = "images/" + imgName
+imageFilenames.forEach(function (imgName) {
+  let img = document.createElement('img')
+  img.src = 'images/' + imgName
   images[imgName] = img
 })
 
 // This section is where you will be doing most of your coding
 class Enemy {
-  constructor(root, xPos) {
+  constructor (root, xPos) {
     this.root = root
     this.x = xPos
     this.y = -ENEMY_HEIGHT
-    let img = document.createElement("img")
-    img.src = "images/enemy.png"
-    img.style.position = "absolute"
-    img.style.left = this.x + "px"
-    img.style.top = this.y + "px"
+    let img = document.createElement('img')
+    img.src = 'images/enemy.png'
+    img.style.position = 'absolute'
+    img.style.left = this.x + 'px'
+    img.style.top = this.y + 'px'
     img.style.zIndex = 5
     root.appendChild(img)
 
@@ -46,33 +46,33 @@ class Enemy {
     this.speed = Math.random() / 2 + 0.25
   }
 
-  update(timeDiff) {
+  update (timeDiff) {
     this.y = this.y + timeDiff * this.speed
   }
 
-  render(ctx) {
-    this.domElement.style.left = this.x + "px"
-    this.domElement.style.top = this.y + "px"
+  render (ctx) {
+    this.domElement.style.left = this.x + 'px'
+    this.domElement.style.top = this.y + 'px'
   }
 
-  destroy() {
+  destroy () {
     // When an enemy reaches the end of the screen, the corresponding DOM element should be destroyed
     this.root.removeChild(this.domElement)
   }
 }
 
 class Player {
-  constructor(root) {
+  constructor (root) {
     this.root = root
     this.x = 2 * PLAYER_WIDTH
     this.y = GAME_HEIGHT - PLAYER_HEIGHT - 10
 
-    let img = document.createElement("img")
-    img.src = "images/player.png"
-    img.style.position = "absolute"
-    img.style.left = this.x + "px"
-    img.style.top = this.y + "px"
-    img.style.zIndex = "10"
+    let img = document.createElement('img')
+    img.src = 'images/player.png'
+    img.style.position = 'absolute'
+    img.style.left = this.x + 'px'
+    img.style.top = this.y + 'px'
+    img.style.zIndex = '10'
 
     root.appendChild(img)
 
@@ -80,7 +80,7 @@ class Player {
   }
 
   // This method is called by the game engine when left/right arrows are pressed
-  move(direction) {
+  move (direction) {
     if (direction === MOVE_LEFT && this.x > 0) {
       this.x = this.x - PLAYER_WIDTH
     } else if (direction === MOVE_RIGHT && this.x < GAME_WIDTH - PLAYER_WIDTH) {
@@ -88,28 +88,30 @@ class Player {
     }
   }
 
-  render(ctx) {
-    this.domElement.style.left = this.x + "px"
-    this.domElement.style.top = this.y + "px"
+  render (ctx) {
+    this.domElement.style.left = this.x + 'px'
+    this.domElement.style.top = this.y + 'px'
   }
 }
 
 class Text {
-  constructor(root, xPos, yPos) {
+  constructor (root, xPos, yPos) {
     this.root = root
 
-    let span = document.createElement("span")
-    span.style.position = "absolute"
+    let span = document.createElement('span')
+    span.style.position = 'absolute'
     span.style.left = xPos
     span.style.top = yPos
-    span.style.font = "bold 30px Impact"
+    span.style.zIndex = 10
+    span.style.color = 'beige'
+    span.style.font = 'bold 30px Impact'
 
     root.appendChild(span)
     this.domElement = span
   }
 
   // This method is called by the game engine when left/right arrows are pressed
-  update(txt) {
+  update (txt) {
     this.domElement.innerText = txt
   }
 }
@@ -119,7 +121,7 @@ This section is a tiny game engine.
 This engine will use your Enemy and Player classes to create the behavior of the game.
 */
 class Engine {
-  constructor(element) {
+  constructor (element) {
     this.root = element
     // Setup the player
     this.player = new Player(this.root)
@@ -129,20 +131,20 @@ class Engine {
     this.setupEnemies()
 
     // Put a white div at the bottom so that enemies seem like they dissappear
-    let whiteBox = document.createElement("div")
+    let whiteBox = document.createElement('div')
     whiteBox.style.zIndex = 100
-    whiteBox.style.position = "absolute"
-    whiteBox.style.top = GAME_HEIGHT + "px"
-    whiteBox.style.height = ENEMY_HEIGHT + "px"
-    whiteBox.style.width = GAME_WIDTH + "px"
-    whiteBox.style.background = "#fff"
+    whiteBox.style.position = 'absolute'
+    whiteBox.style.top = GAME_HEIGHT + 'px'
+    whiteBox.style.height = ENEMY_HEIGHT + 'px'
+    whiteBox.style.width = GAME_WIDTH + 'px'
+    whiteBox.style.background = '#fff'
     this.root.append(whiteBox)
 
-    let bg = document.createElement("img")
-    bg.src = "images/stars.png"
-    bg.style.position = "absolute"
-    bg.style.height = GAME_HEIGHT + "px"
-    bg.style.width = GAME_WIDTH + "px"
+    let bg = document.createElement('img')
+    bg.src = 'images/stars.png'
+    bg.style.position = 'absolute'
+    bg.style.height = GAME_HEIGHT + 'px'
+    bg.style.width = GAME_WIDTH + 'px'
     this.root.append(bg)
 
     // Since gameLoop will be called out of context, bind it once here.
@@ -150,16 +152,16 @@ class Engine {
   }
 
   /*
-     The game allows for 5 horizontal slots where an enemy can be present.
-     At any point in time there can be at most MAX_ENEMIES enemies otherwise the game would be impossible
+    The game allows for 5 horizontal slots where an enemy can be present.
+    At any point in time there can be at most MAX_ENEMIES enemies otherwise the game would be impossible
      */
-  setupEnemies() {
+  setupEnemies () {
     if (!this.enemies) {
       this.enemies = []
     }
 
     while (
-      this.enemies.filter(function() {
+      this.enemies.filter(function () {
         return true
       }).length < MAX_ENEMIES
     ) {
@@ -168,10 +170,10 @@ class Engine {
   }
 
   // This method finds a random spot where there is no enemy, and puts one in there
-  addEnemy() {
+  addEnemy () {
     let enemySpots = GAME_WIDTH / ENEMY_WIDTH
 
-    let enemySpot = undefined
+    let enemySpot
     // Keep looping until we find a free enemy spot at random
     while (!enemySpot || this.enemies[enemySpot]) {
       enemySpot = Math.floor(Math.random() * enemySpots)
@@ -181,10 +183,10 @@ class Engine {
   }
 
   // This method kicks off the game
-  start() {
+  start () {
     this.score = 0
     this.lastFrame = Date.now()
-    let keydownHandler = function(e) {
+    let keydownHandler = function (e) {
       if (e.keyCode === LEFT_ARROW_CODE) {
         this.player.move(MOVE_LEFT)
       } else if (e.keyCode === RIGHT_ARROW_CODE) {
@@ -193,7 +195,7 @@ class Engine {
     }
     keydownHandler = keydownHandler.bind(this)
     // Listen for keyboard left/right and update the player
-    document.addEventListener("keydown", keydownHandler)
+    document.addEventListener('keydown', keydownHandler)
 
     this.gameLoop()
   }
@@ -208,7 +210,7 @@ class Engine {
     To account for the fact that we don't always have 60 frames per second, gameLoop will send a time delta argument to `update`
     You should use this parameter to scale your update appropriately
      */
-  gameLoop() {
+  gameLoop () {
     // Check how long it's been since last frame
     let currentFrame = Date.now()
     let timeDiff = currentFrame - this.lastFrame
@@ -217,13 +219,13 @@ class Engine {
     this.score += timeDiff
 
     // Call update on all enemies
-    this.enemies.forEach(function(enemy) {
+    this.enemies.forEach(function (enemy) {
       enemy.update(timeDiff)
     })
 
     // Draw everything!
-    //this.ctx.drawImage(images["stars.png"], 0, 0); // draw the star bg
-    let renderEnemy = function(enemy) {
+    // this.ctx.drawImage(images["stars.png"], 0, 0); // draw the star bg
+    let renderEnemy = function (enemy) {
       enemy.render(this.ctx)
     }
     renderEnemy = renderEnemy.bind(this)
@@ -242,7 +244,7 @@ class Engine {
     // Check if player is dead
     if (this.isPlayerDead()) {
       // If they are dead, then it's game over!
-      this.info.update(this.score + " GAME OVER")
+      this.info.update(this.score + ' GAME OVER')
     } else {
       // If player is not dead, then draw the score
       this.info.update(this.score)
@@ -253,12 +255,12 @@ class Engine {
     }
   }
 
-  isPlayerDead() {
+  isPlayerDead () {
     // TODO: fix this function!
     return false
   }
 }
 
 // This section will start the game
-let gameEngine = new Engine(document.getElementById("app"))
+let gameEngine = new Engine(document.getElementById('app'))
 gameEngine.start()
